157dc859e4abebbe25008bfeef613855c343cca4
Андрей <andrey.alekseevich.22@yandex.ru>
2022-11-12 19:19:11 +0300

    add footer and some interactive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 const burger = document.querySelector("#burger");
+const emailInputEl = document.querySelector("#input-email");
+const footerButton = document.querySelector("#footer-btn");
 
 burger.addEventListener("click", () => {
     document.querySelector(".burger-list").classList.toggle("none");
 });
+
+function inputClick() {
+    emailInputEl.value = "";
+    emailInputEl.setAttribute("placeholder", "Succesfully");
+    document.getElementById("input-img").classList.remove('none');
+    document.getElementById("input-img").classList.add('animation')
+}
+
+footerButton.addEventListener("click", () => {
+    inputClick();
+    setTimeout(() => {
+        emailInputEl.setAttribute("placeholder", "Your email")
+        document.getElementById("input-img").classList.add('none');
+        document.getElementById("input-img").classList.remove('animation');
+    }, 4000);
+});
+
+

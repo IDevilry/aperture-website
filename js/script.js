f710250b83878aaf4e3b9ceeea2065223539a691
Андрей <andrey.alekseevich.22@yandex.ru>
2022-11-13 16:48:46 +0300

    add modal window

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const burger = document.querySelector("#burger");
 const emailInputEl = document.querySelector("#input-email");
 const footerButton = document.querySelector("#footer-btn");
+const allModalButtons = document.querySelectorAll(".modal-button");
+const allCardsButtons = document.querySelectorAll("[data-name='card-button-m']");
+const getInTouchBtn = document.querySelector("#get-in-touch-btn");
 
 burger.addEventListener("click", () => {
     document.querySelector(".burger-list").classList.toggle("none");
@@ -20,4 +23,21 @@ footerButton.addEventListener("click", () => {
         document.getElementById("input-img").classList.add("none");
         document.getElementById("input-img").classList.remove("animation");
     }, 4000);
+});
+
+allModalButtons.forEach((el) => {
+    el.addEventListener("click", function () {
+        this.parentNode.parentNode.parentNode.classList.add("none");
+    });
+});
+
+allCardsButtons.forEach((el) => {
+    el.addEventListener("click", function () {
+        const id = `${this.id.split("-")[2]}`;
+        document.getElementById(id).parentNode.classList.remove("none");
+    });
+});
+
+getInTouchBtn.addEventListener("click", function () {
+    document.getElementById('get-in-touch').parentNode.classList.remove('none')
 });

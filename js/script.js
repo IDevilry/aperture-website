1575444304f8e871d094558696c412f569d5b386
Андрей <andrey.alekseevich.22@yandex.ru>
2022-11-14 22:38:10 +0300

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 const burger = document.querySelector("#burger");
-const emailInputEl = document.querySelector("#input-email");
+const emailInputEl = document.getElementById("input-email");
 const footerButton = document.querySelector("#footer-btn");
 const allModalButtons = document.querySelectorAll(".modal-button");
 const allCardsButtons = document.querySelectorAll("[data-name='card-button-m']");
 const getInTouchBtn = document.querySelector("#get-in-touch-btn");
 const modalGetInBtn = document.querySelector("#modal-get-in-btn");
+const navLinks = document.querySelectorAll('.nav-item')
 
 burger.addEventListener("click", () => {
     document.querySelector(".burger-list").classList.toggle("burger-active");
 });
 
+navLinks.forEach((element) => {
+    element.addEventListener("click", () => {
+        document.querySelector(".burger-list").classList.remove("burger-active");
+    })
+})
+
 function inputClick() {
+    if (emailInputEl.value == "") {
+        alert('Please enter a valid email');
+        return null;
+    }
     emailInputEl.value = "";
     emailInputEl.setAttribute("placeholder", "Succesfully");
     document.getElementById("input-img").classList.remove("none");
